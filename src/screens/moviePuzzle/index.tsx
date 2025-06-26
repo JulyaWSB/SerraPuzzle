bfac57973e7cedd3936495f9f12c51e0365de626
João Pedro Dias Rodrigues <joaopedrodias890@gmail.com>
2025-06-25 22:15:38 -0300

    const para mostrar as opções de resposta

--- src/screens/moviePuzzle/index.tsx
+++ src/screens/moviePuzzle/index.tsx
@@ -3,14 +3,14 @@ import { View, Text, Button, ActivityIndicator, Alert } from 'react-native';
 import { getGenres, getPopularMovies } from '../../services/MovieApi/movieApiIndex';
 
 
-type Genre = {
+type Genero = {
   id: number;
   name: string;
 };
 
 export  function MoviePuzzle() {
   const [movie, setMovie] = useState<any>(null);
-  const [genres, setGenres] = useState<Genre[]>([]);
+  const [genres, setGenres] = useState<Genero[]>([]);
   const [loading, setLoading] = useState(true);
   const [vidas, setVidas] = useState(3);
   const [acertos, setAcertos] = useState(0);
@@ -28,6 +28,24 @@ export  function MoviePuzzle() {
       console.error('Erro ao buscar gêneros:', error);
     }
   };
+
+  const getOpcoesDeGenero = (): Genero[] => {
+  if (!movie || genres.length === 0) return [];
+
+  const generosDoFilme = genres.filter((g) => movie.genreIds.includes(g.id));
+  const generosErrados = genres.filter((g) => !movie.genreIds.includes(g.id));
+
+  const generosAleatoriosErrados = generosErrados
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 3); // 3 errados
+
+  const generoCorreto = generosDoFilme[0]; // pegar o primeiro gênero do filme
+
+  const opcoes = [...generosAleatoriosErrados, generoCorreto]
+    .sort(() => Math.random() - 0.5); // embaralhar as opções
+
+  return opcoes;
+};
 
   const sortearFilme = async () => {
   try {
